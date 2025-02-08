b73b3b87a90226b9f6cb6a037bd37dcf82568147
Heitor Cardozo <heitor711n@gmail.com>
2025-02-08 11:35:48 -0300

    feat: create config setup

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	var err error
+
+	err = LoadEnvVariables()
+
+	if err != nil {
+		return err
+	}
+
+	db, err = ConnectPostgres()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetPostgres() *gorm.DB {
+	return db
+}
+
+func GetLogger(prefix string) *Logger {
+	logger = NewLogger(prefix)
+	return logger
+}
---
+++ config/postgres.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"os"
+
+	"gorm.io/gorm"
+
+	"gorm.io/driver/postgres"
+)
+
+func ConnectPostgres() (*gorm.DB, error) {
+	logger := GetLogger("postgreSQL")
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Info("Database connection established successfully")
+
+	return db, nil
+}
