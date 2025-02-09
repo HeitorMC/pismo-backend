4e62c9b6f0a2353c2c65301872ab63ffcee21418
Heitor Cardozo <heitor711n@gmail.com>
2025-02-09 18:18:27 -0300

    feat: add Swagger configuration

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,8 +1,11 @@
 package router
 
 import (
+	docs "github.com/HeitorMC/pismo-backend/docs"
 	"github.com/HeitorMC/pismo-backend/internal/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
@@ -10,4 +13,9 @@ func initializeRoutes(router *gin.Engine) {
 	router.POST("/accounts", handler.CreateAccount)
 	router.GET("/accounts/:accountId", handler.ShowAccount)
 	router.POST("/transactions", handler.CreateTransaction)
+
+	docs.SwaggerInfo.Title = "Pismo Backend"
+	docs.SwaggerInfo.Description = "This is a simple API to create accounts and transactions"
+	docs.SwaggerInfo.Version = "1.0"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
